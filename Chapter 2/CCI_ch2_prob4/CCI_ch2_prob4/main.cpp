3e3b5dc7298da481c8502bbbbd317c301b66c6a7
Viper-PC\Viper <kreppsjs@gmail.com>
2016-09-21 17:44:58 -0500

    genesis ch2prob4

---
+++ Chapter 2/CCI_ch2_prob4/CCI_ch2_prob4/main.cpp
@@ -0,0 +1,13 @@
+/* Cracking the Coding Interview, McDowell 5th Ed, 2011
+   Chapter 2: Problem 4.
+   Write code to partition a linked list around a value
+   x, such that all nodes less than x come before all nodes
+   greater than or equal to x.
+   
+   Solutions by: vnpjeremy*/
+
+#include "SLList.h"
+int main()
+{
+
+}
