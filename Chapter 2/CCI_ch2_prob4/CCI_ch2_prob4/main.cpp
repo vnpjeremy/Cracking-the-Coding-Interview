4d1e5282fd287b80205424c6ad6e5a2c6994ece7
Viper-PC\Viper <kreppsjs@gmail.com>
2016-09-21 19:39:34 -0500

    wait in-place or with buffer

--- Chapter 2/CCI_ch2_prob4/CCI_ch2_prob4/main.cpp
+++ Chapter 2/CCI_ch2_prob4/CCI_ch2_prob4/main.cpp
@@ -7,7 +7,41 @@
    Solutions by: vnpjeremy*/
 
 #include "SLList.h"
+
+//NOTE: Put templates in headers, but for clarity in this exercise, putting 
+//it in the implementation file in global scope. (Don't do this.)
+
+//will be linear, but won't be sorted.
+template <class T>
+void SLList<T>::partition(T const& data)
+{
+    if(!m_head)
+        return;
+
+    SLList<T>::Node<T>* node = find(data);
+    if(!node)
+        return;
+
+    SLList<T>::Node<T>* cur = m_head;
+    while(cur)
+    {
+        //less, then head. Else, tail.
+        if(cur->m_data < node->m_data)
+        {
+            SLList<T>::Node<T>* newNode = new SLList<T>::Node<T>;
+        }
+        else
+        {
+
+        }
+    }
+}
+
 int main()
 {
+    //Develop insertion before & after that is constant\
+    //push_back: straightforward
+    //push_front: rename the head
+
 
 }
