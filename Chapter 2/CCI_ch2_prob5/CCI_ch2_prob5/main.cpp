a51676505e3e5bfd3718d78763486b3788cf1c1c
Viper-PC\Viper <kreppsjs@gmail.com>
2016-09-24 13:27:38 -0500

    While loop version works well. Attemp recursive solution before moving to reverse list storage version.

--- Chapter 2/CCI_ch2_prob5/CCI_ch2_prob5/main.cpp
+++ Chapter 2/CCI_ch2_prob5/CCI_ch2_prob5/main.cpp
@@ -7,25 +7,50 @@
 
 #include "SLList.h"
 #include <vector>
-//#include <iterator>
 
 //As noted previously, DO NOT put templates in global scope in implementation
 //files. Put in headers.
 
-//could be made a binary operator()
+/* Will add the trailing nodes first, then proceed
+   to leading ones. Leading nodes may not align. */
 
-//enable_if for integral, floating_point types??
-template <class T>
+/* This is an advantageous way of storing integers
+   as the inverse would require one to reverse-traverse
+   the LL, an extremely inefficent process unless double-linkage
+   exists. */
+template <class T, typename = std::enable_if_t<std::is_integral<T>::value>>
 SLList<T> add( SLList<T> & lhs, SLList<T> & rhs )
 {
-    /* Will add the trailing nodes first, then proceed
-       to leading ones. Leading nodes may not align. */
+    SLList<T>::Node<T> const *curLHS = lhs.m_head, *curRHS = rhs.m_head;
+    SLList<T>      output;
+    T carry = {};
+    while(curLHS || curRHS)
+    {
+        T const lhsVal = curLHS ? curLHS->m_data : 0;
+        T const rhsVal = curRHS ? curRHS->m_data : 0;
+        T const sum = lhsVal + rhsVal + carry;
+        T const answer = sum % 10;
+        carry = sum > 10 ? 1 : 0;        
+        output.push_back(answer);
 
-    /* This is an advantageous way of storing integers
-       as the inverse would require one to reverse-traverse
-       the LL, an extremely inefficent process unless double-linkage
-       exists. */
+        curLHS = curLHS ? curLHS->m_next : curLHS;
+        curRHS = curRHS ? curRHS->m_next : curRHS;
+    }
 
+    return output;
+}
+
+template <class T, typename = std::enable_if_t<std::is_integral<T>::value>>
+SLList<T> addRecursive(SLList<T> & lhs, SLList<T> & rhs)
+{
+    SLList<T>      output;
+    return output;
+}
+
+template <class T>
+SLList<T> addForwardFormat(SLList<T> & lhs, SLList<T> & rhs)
+{
+    /* For non-reverse storage in LL containers: */
     /* Padding will be faster if .size() is O(1) instead of O(n). */
     size_t const size1 = lhs.size(), size2 = rhs.size();
     if(size1 < size2)
@@ -43,50 +68,28 @@ SLList<T> add( SLList<T> & lhs, SLList<T> & rhs )
             rhs.push_front(val);
     }
 
-    SLList<T>::Node<T> const *curLHS = lhs.m_head, *curRHS = rhs.m_head;
-    SLList<T>      output;
-    while(curLHS || curRHS)
-    {
-        //T carry
-        if(curLHS && curRHS)
-        {
-            output.push_front(curLHS->m_data + curRHS->m_data);
-            curLHS = curLHS->m_next;
-            curRHS = curRHS->m_next;
-        }
-        else if(curLHS)
-        {
-            output.push_front(curLHS->m_data);
-            curLHS = curLHS->m_next;
-        }
-        else
-        {
-            output.push_front(curRHS->m_data);
-            curRHS = curRHS->m_next;
-        }
-    }
 
-    return output;
+    return SLList<T>();
 }
 
 int main()
 {
-    int arg1();
-    int arg2 = 0;
-   // int arg3 = arg1 + arg2;
-
     SLList<int> num1, num2; 
     num1.push_front(1);
     num1.push_front(0);
     num1.push_front(6);
 
-    num2.push_front(5);
     num2.push_front(6);
-    std::vector<int> flat0  = num1.flatten();
-    std::vector<int> flat00 = num2.flatten();
+    std::vector<int> verify1  = num1.flatten();
+    std::vector<int> verify2 = num2.flatten();
 
     SLList<int>      output = add(num1, num2);
-    std::vector<int> flat1 = output.flatten();
+    std::vector<int> verify3 = output.flatten();
+
+    SLList<double> disabledListType1, disabledListType2;
+    disabledListType1.push_back(2.01);
+    disabledListType2.push_back(1.05);
+    //SLList<double> shouldntWork = add(disabledListType1, disabledListType2);
 
     int dummy = 0;
 }
