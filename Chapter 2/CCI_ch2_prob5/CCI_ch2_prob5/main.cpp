8af2232df10962230de346b207b9f46550ce819a
Viper-PC\Viper <kreppsjs@gmail.com>
2016-09-24 15:37:58 -0500

    Ignoring recursion. Doesn't make sense. Neither however does reverse-traversing a singly linked list.

--- Chapter 2/CCI_ch2_prob5/CCI_ch2_prob5/main.cpp
+++ Chapter 2/CCI_ch2_prob5/CCI_ch2_prob5/main.cpp
@@ -3,7 +3,9 @@
    
    You have 2 numbers represented by a LL. Each node is a digit.
    They are in reverse order. Write an addition function that retuns
-   the result as a LL. */
+   the result as a LL. 
+   
+   Part 2: the digits are in forward order. Repeat.*/
 
 #include "SLList.h"
 #include <vector>
@@ -19,7 +21,7 @@
    the LL, an extremely inefficent process unless double-linkage
    exists. */
 template <class T, typename = std::enable_if_t<std::is_integral<T>::value>>
-SLList<T> add( SLList<T> & lhs, SLList<T> & rhs )
+SLList<T> add( SLList<T> const& lhs, SLList<T> const& rhs )
 {
     SLList<T>::Node<T> const *curLHS = lhs.m_head, *curRHS = rhs.m_head;
     SLList<T>      output;
@@ -41,15 +43,18 @@ SLList<T> add( SLList<T> & lhs, SLList<T> & rhs )
 }
 
 template <class T, typename = std::enable_if_t<std::is_integral<T>::value>>
-SLList<T> addRecursive(SLList<T> & lhs, SLList<T> & rhs)
+void addRecursive(SLList<T> const& lhs, SLList<T> const& rhs, SLList<T> & output)
 {
-    SLList<T>      output;
-    return output;
+    //The book solves this recursively, but that doesn't make sense to me.
+    //It feels like shoehorning the solution in when a perfectly logical
+    //loop approach would work fine. Will try if there is time.
 }
+
 
 template <class T>
 SLList<T> addForwardFormat(SLList<T> & lhs, SLList<T> & rhs)
 {
+#if 0
     /* For non-reverse storage in LL containers: */
     /* Padding will be faster if .size() is O(1) instead of O(n). */
     size_t const size1 = lhs.size(), size2 = rhs.size();
@@ -67,10 +72,11 @@ SLList<T> addForwardFormat(SLList<T> & lhs, SLList<T> & rhs)
         for(size_t ii = 0; ii < diff; ++ii)
             rhs.push_front(val);
     }
-
+#endif
 
     return SLList<T>();
 }
+
 
 int main()
 {
@@ -90,6 +96,18 @@ int main()
     disabledListType1.push_back(2.01);
     disabledListType2.push_back(1.05);
     //SLList<double> shouldntWork = add(disabledListType1, disabledListType2);
+
+    SLList<int> num3, num4;
+    num3.push_back(1);
+    num3.push_back(0);
+    num3.push_back(6);
+
+    num4.push_front(6);
+    std::vector<int> verify5 = num3.flatten();
+    std::vector<int> verify6 = num4.flatten();
+
+    SLList<int>      output2 = addForwardFormat(num1, num2);
+    std::vector<int> verify7 = output.flatten();
 
     int dummy = 0;
 }
