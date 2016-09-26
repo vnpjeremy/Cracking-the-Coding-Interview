dbf4687eab0dac27dec6893c208a386dcb027259
Viper-PC\Viper <kreppsjs@gmail.com>
2016-09-25 19:34:46 -0500

    ch2 prob7 genesis

---
+++ Chapter 2/CCI_ch2_prob7/CCI_ch2_prob7/main.cpp
@@ -0,0 +1,11 @@
+/* Cracking the Coding Interview, McDowell 5th Ed, 2011
+   Chapter 2: Problem 7.
+
+   Check to see if a LL is a palindrome.
+   
+   Solutions by: vnpjeremy*/
+
+int main()
+{
+
+}
