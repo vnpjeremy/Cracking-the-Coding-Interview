3bfbf0fbd363f8c5d610432c5cf19519ffbb6a4b
Viper-PC\Viper <kreppsjs@gmail.com>
2016-09-26 18:12:45 -0500

    Implement O(n) time O(1) space reverse method for SLList

---
+++ Chapter 2/CCI_ch2_prob7/CCI_ch2_prob7/SLList.h
@@ -0,0 +1,233 @@
+#pragma once
+#pragma once
+#include <vector>
+
+template <class T>
+class SLList
+{
+public:
+    template <class T>
+    class Node
+    {
+    public:
+        Node(T data) :
+            m_next(),
+            m_data(data)
+        {
+        }
+
+        Node() :
+            m_next(),
+            m_data()
+        {
+        }
+
+        Node(Node<T> const& rhs)
+        {
+            m_next = rhs.m_next;
+            m_data = rhs.m_data;
+        }
+
+        Node<T>& operator=(Node<T> const& rhs)
+        {
+            if(this != &rhs)
+            {
+                m_next = rhs.m_next;
+                m_data = rhs.m_data;
+            }
+            return *this;
+        }
+
+        Node<T>* m_next;
+        T        m_data;
+    };
+
+public:
+    SLList() :
+        m_head(),
+        m_tail()
+    {
+    }
+
+    void removeNode(T const& data);
+    void removeNode(Node<T>* node);
+
+    Node<T>* find(T const& data) const//const&?
+    {
+        Node<T>* cur = m_head;
+        while(cur)
+        {
+            if(cur->m_data == data)
+                return cur;
+            cur = cur->m_next;
+        }
+        return cur;
+    }
+
+    //Rule of 3/5: Need a CC
+    SLList(SLList<T> const& rhs)
+    {
+        Node<T>* cur = rhs.m_head;
+        while(cur)
+        {
+            push_back(cur->m_data);
+            cur = cur->m_next;
+        }
+    }
+
+    //Rule of 3/5: Need a CA
+    SLList<T>& operator=(SLList<T> const& rhs)
+    {
+        if(this != rhs)
+        {
+            Node<T>* cur = rhs.m_head;
+            while(cur)
+            {
+                push_back(cur->m_data);
+                cur = cur->m_next;
+            }
+        }
+        return *this;
+    }
+    //MA
+
+    //MC
+
+    void push_back(T const& data)
+    {
+        Node<T>* newNode = new Node<T>(data);
+        if(!m_head)
+        {
+            m_head = newNode;
+            m_tail = m_head;
+        }
+        else
+        {
+            m_tail->m_next = newNode;
+            m_tail = newNode;
+        }
+    }
+
+    void push_front(T const& data)
+    {
+        Node<T>* newNode = new Node<T>(data);
+        if(!m_head)
+            m_head = newNode;
+        else
+        {
+            newNode->m_next = m_head;
+            m_head = newNode;
+        }
+    }
+
+    size_t size() const
+    {
+        size_t size = 0;
+        Node<T>* cur = m_head;
+        while(cur)
+        {
+            ++size;
+            cur = cur->m_next;
+        }
+        return size;
+    }
+
+    //bool hasCircularLinkage(Node<T> & repeated) const;
+
+    void reverse();
+
+    std::vector<T> flatten() const
+    {
+        std::vector<T> output;
+        Node<T>* tmp = m_head;
+        while(tmp)
+        {
+            output.push_back(tmp->m_data);
+            tmp = tmp->m_next;
+        }
+
+        return output;
+    }
+
+    void clear()
+    {
+#if 0
+        Node<T> repeat;
+        if(hasCircularLinkage(repeat))
+            return; //leak memory for now. Corruption. Destructors should not
+                    //throw, but something will have to give here.
+#endif
+
+        while(m_head)
+        {
+            Node<T>* tmp = m_head->m_next;
+            delete m_head;
+            m_head = tmp;
+        }
+    }
+
+    ~SLList()
+    {
+        clear();
+    }
+
+    Node<T>* m_head;
+    Node<T>* m_tail;
+};
+
+template <class T>
+void SLList<T>::removeNode(T const& data)
+{
+    Node<T>* removeCandidate = find(data);
+    if(!removeCandidate)
+        return;
+
+    if(!removeCandidate->m_next)
+    {
+        Node<T>* tmp = m_head;
+        while(tmp)
+        {
+            if(tmp->m_next->m_data == removeCandidate->m_data)
+                break;
+            tmp = tmp->m_next;
+        }
+
+        delete removeCandidate;
+        tmp->m_next = nullptr;
+        m_tail = tmp;
+        return;
+    }
+    //special case; Won't be O(1) time for this guy. Probably.
+    //Can easily do O(n), naively.
+
+    Node<T>* tmp = removeCandidate->m_next;
+    removeCandidate->m_data = removeCandidate->m_next->m_data;
+    removeCandidate->m_next = removeCandidate->m_next->m_next;
+    delete tmp;
+}
+
+template <class T>
+void SLList<T>::removeNode(Node<T>* removeCandidate)
+{
+    if(!removeCandidate->m_next)
+    {
+        Node<T>* tmp = m_head;
+        while(tmp)
+        {
+            if(tmp->m_next->m_data == removeCandidate->m_data)
+                break;
+            tmp = tmp->m_next;
+        }
+
+        delete removeCandidate;
+        tmp->m_next = nullptr;
+        m_tail = tmp;
+        return;
+    }//Won't be O(1) time for this guy. Probably.
+     //Can easily do O(n), naively.
+
+    Node<T>* tmp = removeCandidate->m_next;
+    removeCandidate->m_data = removeCandidate->m_next->m_data;
+    removeCandidate->m_next = removeCandidate->m_next->m_next;
+    delete tmp;
+}
--- Chapter 2/CCI_ch2_prob7/CCI_ch2_prob7/main.cpp
+++ Chapter 2/CCI_ch2_prob7/CCI_ch2_prob7/main.cpp
@@ -5,7 +5,33 @@
    
    Solutions by: vnpjeremy*/
 
+#include "SLList.h"
+
+/* O(n) time O(1) space reversal method */
+template <class T>
+inline void SLList<T>::reverse()
+{
+    SLList<T>::Node<T> *cur = m_head, *prev = nullptr, *tmp = m_head;
+    m_head = m_tail;
+    m_tail = tmp;
+    while(cur)
+    {
+        SLList<T>::Node<T>* tmp = cur->m_next;
+        cur->m_next = prev; //reverse the linkage
+        prev = cur;         //update prev iterator
+        cur = tmp;          //update cur iterator
+    }
+}
+
 int main()
 {
+    SLList<int> list1;
+    for(int ii = 0; ii < 7; ++ii)
+        list1.push_back(ii);
 
+    std::vector<int> test1 = list1.flatten();
+    list1.reverse();
+    std::vector<int> test2 = list1.flatten();
+
+    int dummy = 0;
 }
