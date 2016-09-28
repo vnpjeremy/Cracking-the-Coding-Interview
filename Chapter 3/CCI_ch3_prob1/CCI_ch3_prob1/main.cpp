aec319f56ebd9953d3a6683563343080559048ac
Viper-PC\Viper <kreppsjs@gmail.com>
2016-09-28 16:37:56 -0500

    ch3 prob1 genesis

---
+++ Chapter 3/CCI_ch3_prob1/CCI_ch3_prob1/main.cpp
@@ -0,0 +1,12 @@
+/* Cracking the Coding Interview, McDowell 5th Ed, 2011
+   Chapter 3: Problem 1. 
+
+   How could you use a single array to implement three stacks?
+
+   Solutions by vnpjeremy.
+*/
+
+int main()
+{
+
+}
