d04f0a1d67d0fcaeb94c20db3420c8f27a236001
Viper-PC\Viper <kreppsjs@gmail.com>
2016-09-28 16:41:10 -0500

    Ch3 prob2 genesis

---
+++ Chapter 3/CCI_ch3_prob2/CCI_ch3_prob2/main.cpp
@@ -0,0 +1,12 @@
+/* Cracking the Coding Interview, McDowell 5th Ed, 2011
+   Chapter 2: Problem 2.
+   
+   Design a stack which has a function min() operating in O(1) time.
+   Push and Pop should also be O(1) time.
+   
+   Solutions by: vnpjeremy*/
+
+int main()
+{
+
+}
