48c90ef21bb07663fb0b5f2fb179d66c939214a6
Viper-PC\Viper <kreppsjs@gmail.com>
2016-10-02 16:12:57 -0500

    Implementation works, but popAt not addressed yet

--- Chapter 3/CCI_ch3_prob3/CCI_ch3_prob3/SpillStack.h
+++ Chapter 3/CCI_ch3_prob3/CCI_ch3_prob3/SpillStack.h
@@ -6,6 +6,12 @@
 /* If we don't have an m_size data member on our Stack class,
    it may be advantageous to add it so that .size() is O(1) and 
    not O(n) in this type of use scenario. */
+
+/* Point of consideration:
+   This design does not 'shrink', that is, when a large stack
+   is populated, popping the thing down to zero won't delete
+   the elements of the vector. Deleting elements from a vector
+   can be less than optimal, so add that in if desired. */
 template <class T>
 class SpillStack
 {
@@ -17,31 +23,53 @@ public:
     SpillStack() :
         m_maxStackSize(defaultMaxSize),
         m_stackIndex(),
-        m_stacks()
+        m_stacks(1) //Populate the first stack in the empty vector
     {
     }
 
     SpillStack(size_t const maxSize) :
         m_maxStackSize(maxSize),
         m_stackIndex(),
-        m_stacks()
+        m_stacks(1)
     {
     }
 
     void push(T const& data)
     {
+        assert(!m_stacks.empty());
         if(m_stacks[m_stackIndex].size() < m_maxStackSize)
             m_stacks[m_stackIndex].push(data);
         else
         {
-            m_stacks.push_back(std::stack<T>());
+            /* stack may exist and be empty from previous operations */
+            if(m_stacks.size() < m_stackIndex + 2)
+                m_stacks.push_back(std::stack<T>());
             m_stacks[++m_stackIndex].push(data);
         }
+    }
+    
+    T & top()
+    {
+        assert(!m_stacks[m_stackIndex].empty());
+        return m_stacks[m_stackIndex].top();
+    }
+
+    T const& top() const
+    {
+        assert(!m_stacks[m_stackIndex].empty());
+        return m_stacks[m_stackIndex].top();
     }
 
     void pop()
     {
+        assert(!m_stacks[0].empty());
+        if(m_stacks[m_stackIndex].empty())
+            --m_stackIndex;
+
         assert(!m_stacks[m_stackIndex].empty());
+        m_stacks[m_stackIndex].pop();
+        if(m_stacks[m_stackIndex].empty() && m_stackIndex > 0) //we just popped the last one
+            --m_stackIndex;
     }
 
     /* CAUTION: This is debug-only, and for stacks!
--- Chapter 3/CCI_ch3_prob3/CCI_ch3_prob3/main.cpp
+++ Chapter 3/CCI_ch3_prob3/CCI_ch3_prob3/main.cpp
@@ -6,6 +6,9 @@
    another stack is begun. Push() and Pop() should behave the same
    as with a regular stack.
 
+   Follow-up: Implement (or talk through the implementation of) a function
+   popAt(int index) which performs a pop operation on a sub-stack.
+
    Solutions by vnpjeremy.
 */
 
@@ -17,10 +20,35 @@
 int main()
 {
     SpillStack<int> testStack1;
-    for(size_t ii = 0; ii < 12; ++ii)
+    for(size_t ii = 0; ii < 6; ++ii)
+        testStack1.push(static_cast<int>(ii));
+    testStack1.pop();
+    for(size_t ii = 5; ii < 12; ++ii)
         testStack1.push(static_cast<int>(ii));
 
-    std::vector<int> out1 = testStack1.printCurrentStack();
+    //try populating 6, popping to 5, and pushing again
+
+    //std::vector<int> out1 = testStack1.printCurrentStack();
+
+    std::vector<int> fullPrint;
+    for(size_t ii = 0; ii < 12; ++ii)
+    {
+        fullPrint.push_back(testStack1.top());
+        testStack1.pop();
+    }
+
+    /* Test re-populating, and then some. Empty stacks should exist. */
+    for(size_t ii = 0; ii < 21; ++ii)
+        testStack1.push(static_cast<int>(ii));
+
+
+    std::vector<int> fullPrint2;
+    for(size_t ii = 0; ii < 21; ++ii)
+    {
+        fullPrint2.push_back(testStack1.top());
+        testStack1.pop();
+    }
+
 
     int dummy = 0;
 }
