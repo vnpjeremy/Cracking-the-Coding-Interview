6ea8c4812bbd119767aee605623555eebc879e1a
Viper-PC\Viper <kreppsjs@gmail.com>
2016-10-03 13:44:42 -0500

    move methods

--- Chapter 3/CCI_ch3_prob4/CCI_ch3_prob4/hanoiStacks.h
+++ Chapter 3/CCI_ch3_prob4/CCI_ch3_prob4/hanoiStacks.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <stack>
-
+#include <cassert>
 //enable_if integral type
 //even IF integral type, what if short, and N > std::numeric_limits<short>::Max()? problem!
 //probably can't/shouldn't templatize.
@@ -9,7 +9,7 @@
 class hanoiStacks
 {
 public:
-    hanoiStacks() :
+    hanoiStacks() noexcept :
         m_numberOfDiscs(3),
         m_stack1(),
         m_stack2(),
@@ -18,7 +18,7 @@ public:
         populateFirstStack();
     }
 
-    hanoiStacks(size_t const discCount) :
+    hanoiStacks(size_t const discCount) noexcept :
         m_numberOfDiscs(discCount),
         m_stack1(),
         m_stack2(),
@@ -27,12 +27,32 @@ public:
         populateFirstStack();
     }
 
-    void populateFirstStack()
+    void populateFirstStack() noexcept
     {
         for(size_t ii = 0; ii < m_numberOfDiscs; ++ii)
             m_stack1.push(ii);
     }
 
+    void moveAtoB()
+    {
+        assert(!m_stack1.empty());
+        m_stack2.push(m_stack1.top());
+        m_stack1.pop();
+    }
+    
+    void moveBtoC()
+    {
+        assert(!m_stack2.empty());
+        m_stack3.push(m_stack2.top());
+        m_stack2.pop();
+    }
+
+    void moveAtoC()
+    {
+        assert(!m_stack1.empty());
+        m_stack3.push(m_stack1.top());
+        m_stack1.pop();
+    }
 
 private:
     /* Could be an array of stack[3] as well. */
