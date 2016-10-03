8add49e903facfe2374f42ce61d6014b6525d2e4
Viper-PC\Viper <kreppsjs@gmail.com>
2016-10-03 10:40:52 -0500

    ch3 prob4 genesis

---
+++ Chapter 3/CCI_ch3_prob4/CCI_ch3_prob4/main.cpp
@@ -0,0 +1,15 @@
+/* Cracking the Coding Interview, McDowell 5th Ed, 2011
+   Chapter 2: Problem 4.
+   
+   Solve the Towers of Hanoi problem using stacks. 3
+   towers, N discs.
+   
+   Solutions by: vnpjeremy*/
+
+
+/* It is well-known that this problem can be solved in 
+   2*N-1 operations. */
+int main()
+{
+
+}
