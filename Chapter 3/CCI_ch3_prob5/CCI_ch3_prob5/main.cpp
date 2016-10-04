fe879f575f2b1e75ffddb54d6b42bf3f608437ed
Viper-PC\Viper <kreppsjs@gmail.com>
2016-10-03 20:49:05 -0500

    ch3 prob 5 genesis

---
+++ Chapter 3/CCI_ch3_prob5/CCI_ch3_prob5/main.cpp
@@ -0,0 +1,12 @@
+/* Cracking the Coding Interview, McDowell 5th Ed, 2011
+   Chapter 2: Problem 5.
+   
+   Implement a queue using two stacks.
+   
+   Solutions by: vnpjeremy*/
+
+
+int main()
+{
+
+}
