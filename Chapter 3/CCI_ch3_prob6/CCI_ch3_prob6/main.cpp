fde462582897caccaf3cba0c0f7d016757f9c090
Viper-PC\Viper <kreppsjs@gmail.com>
2016-10-05 13:46:58 -0500

    ch3 prob6 genesis

---
+++ Chapter 3/CCI_ch3_prob6/CCI_ch3_prob6/main.cpp
@@ -0,0 +1,14 @@
+/* Cracking the Coding Interview, McDowell 5th Ed, 2011
+   Chapter 2: Problem 6.
+   
+   Sort a stack in ascending order - biggest item on top.
+   Make it no worse than O(n) space for another stack,
+   but use no non-identical containers (stacks only).
+   Push, Pop, Top, Empty (like STL).
+   
+   Solutions by: vnpjeremy*/
+
+int main()
+{
+
+}
