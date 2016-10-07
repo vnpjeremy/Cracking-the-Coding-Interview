454ce984ff239ab2b5155af3881d2f01ab03c129
Viper-PC\Viper <kreppsjs@gmail.com>
2016-10-07 17:59:26 -0500

    find pivot - without overflow issues

--- Chapter 11/Searches/QuickSort/QuickSort/QuickSort.h
+++ Chapter 11/Searches/QuickSort/QuickSort/QuickSort.h
@@ -9,9 +9,17 @@
    nukes itself.
 
    Do:
+   [(low + high) + low - low ] / 2
+   [2*low + high - low] / 2
+   -->  low + (high-low) / 2
+*/
 
-
-   */
+//fun thing to get size
+template <class T, size_t N>
+size_t inline size1(T (&arr)[N])
+{
+    return N;
+}
 
 template <class T>
 inline void QuickSort( T            *arr, 
@@ -21,11 +29,15 @@ inline void QuickSort( T            *arr,
     assert(size > 0);
 
     /* 1. Pick a pivot */
+    T   *midPivot = arr + (arr + size - arr) / 2;
+    T   mid = *midPivot;
+
+
 
     /* 2. Partition so that all values less than pivot come before the pivot */
 
     /* 3. Recursively apply above steps to sub-array of elements with smaller than pivot,
           as well as same for greater. */
 
-
+    int dummy = 0;
 }
--- Chapter 11/Searches/QuickSort/QuickSort/main.cpp
+++ Chapter 11/Searches/QuickSort/QuickSort/main.cpp
@@ -1,6 +1,13 @@
 
 /* Test the quicksort impl */
+#include "QuickSort.h"
+
 int main()
 {
+    //std::array<int, 7> = {//stuff};
+    int     arr[] = {4, -15, 25, 61, -88, 91, 2};
+    size_t  len = size1(arr);
+    QuickSort(arr, len);
 
+    int dummy = 0;
 }
