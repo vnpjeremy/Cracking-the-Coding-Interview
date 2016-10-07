bac59b542e17e94d80d62561de655a7016a382a9
Viper-PC\Viper <kreppsjs@gmail.com>
2016-10-07 16:52:35 -0500

    quicksort genesis

---
+++ Chapter 11/Searches/QuickSort/QuickSort/QuickSort.h
@@ -0,0 +1,8 @@
+#pragma once
+
+template <class T>
+inline void QuickSort( T            *arr, 
+                       size_t const size )
+{
+
+}
---
+++ Chapter 11/Searches/QuickSort/QuickSort/main.cpp
@@ -0,0 +1,6 @@
+
+/* Test the quicksort impl */
+int main()
+{
+
+}
