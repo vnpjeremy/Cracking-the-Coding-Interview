3a73dff1f1f5bc1ff8bf825511ff3a953bf541be
Viper-PC\Viper <kreppsjs@gmail.com>
2016-10-11 14:43:30 -0500

    add testing for a bigger array

--- Chapter 11/Sorts & Searches/QuickSort/QuickSort/main.cpp
+++ Chapter 11/Sorts & Searches/QuickSort/QuickSort/main.cpp
@@ -1,10 +1,20 @@
 
 /* Test the quicksort impl */
+#include <random>
 #include "QuickSort.h"
 
 int main()
 {
-    //std::array<int, 7> = {//stuff};
+    std::random_device              rDev;
+    std::mt19937                    gen(rDev());
+    std::uniform_int_distribution<> dogsAndCats(-200, 200);
+    size_t const bigSize = 25;
+    int arr0[bigSize];
+    for(size_t ii = 0; ii < bigSize; ++ii)
+        arr0[ii] = dogsAndCats(gen);
+
+    QuickSort(arr0, 0, bigSize);
+
     int     arr[] = {4, -15, 25, 61, -88, 91, 2, 121};
     size_t  len = size1(arr);
 
