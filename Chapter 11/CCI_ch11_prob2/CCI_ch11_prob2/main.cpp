a7f3c5995b57e0b526c56e1fa1880018e53d6795
Viper-PC\Viper <kreppsjs@gmail.com>
2016-10-14 17:57:04 -0500

    get an array going

--- Chapter 11/CCI_ch11_prob2/CCI_ch11_prob2/main.cpp
+++ Chapter 11/CCI_ch11_prob2/CCI_ch11_prob2/main.cpp
@@ -5,7 +5,27 @@
    
    Solutions by: vnpjeremy*/
 
-int main()
+/* First up we have to generate this nonsense. */
+#include <string>
+#include <vector>
+
+void stringSort(std::vector<std::string> & unsortedArr)
 {
 
+}
+
+int main()
+{
+    std::vector<std::string> stringArr;
+    stringArr.push_back(std::string("bat")); //non-explicit constructor?
+    stringArr.push_back("flash");
+    stringArr.push_back("grab");
+    stringArr.push_back("lighthouse");
+    stringArr.push_back("zephyr");
+    stringArr.push_back("brag");
+    stringArr.push_back("tab");
+
+    stringSort(stringArr);
+
+    int dummy = 0;
 }
