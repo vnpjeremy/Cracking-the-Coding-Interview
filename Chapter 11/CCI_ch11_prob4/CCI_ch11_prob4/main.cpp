6772068d2c837adb30e630df03d4ca5977ae51e6
Viper-PC\Viper <kreppsjs@gmail.com>
2016-10-18 14:01:08 -0500

    ch11 prob4 genesis

---
+++ Chapter 11/CCI_ch11_prob4/CCI_ch11_prob4/main.cpp
@@ -0,0 +1,15 @@
+/* Cracking the Coding Interview, McDowell 5th Ed, 2011
+   Chapter 11: Problem 4.
+   
+   Say you have a 20GB file with one string per line. How
+   would you sort it.
+   
+   Solutions by: vnpjeremy*/
+
+/* Well, first off we don't have 20GB of RAM. At least not as of 2016.
+   This means we should assume we're dealing with spin-disk page-file type
+   behavior, very slow random read/write. */
+int main()
+{
+
+}
