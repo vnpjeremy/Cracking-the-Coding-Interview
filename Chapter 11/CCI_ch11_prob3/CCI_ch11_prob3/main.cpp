67e7a0d2d3b5d7d1086e46c36fdf0b323ff9d4c6
Viper-PC\Viper <kreppsjs@gmail.com>
2016-10-18 12:58:16 -0500

    fix hole in post-pivot lower_bound query. all paths work

--- Chapter 11/CCI_ch11_prob3/CCI_ch11_prob3/main.cpp
+++ Chapter 11/CCI_ch11_prob3/CCI_ch11_prob3/main.cpp
@@ -22,8 +22,12 @@
  
    So we can kind of do this. Ish. For the pivot discovery:
    */
+
+
 template <class T>
-inline size_t findPivot(T const* arrayInput, size_t const LH, size_t const RH)
+inline size_t findPivot( T const*const  arrayInput,
+                         size_t const   LH, 
+                         size_t const   RH )
 {
     /* If 0 is passed, and this is true, we know there isn't a pivot. Assume ordered. */
     //assert(LH != RH); //also assume no repeated elements? Is that fair?
@@ -47,22 +51,26 @@ inline size_t findPivot(T const* arrayInput, size_t const LH, size_t const RH)
 
 
 template <class T>
-inline size_t findElement(T const* arrayInput, size_t const len, T const& elem)
+inline size_t findElement( T const*const  arrayInput, 
+                           size_t const   len, 
+                           T const&       elem )
 {
     size_t const pivot = findPivot(arrayInput, 0, len - 1);
 
     auto itrLHS = std::lower_bound(arrayInput, arrayInput + pivot, elem);
-    if(itrLHS != arrayInput + len)
+    if(itrLHS != arrayInput + len && elem == *itrLHS)
     {
         /* Note: if InputIterator meets the requirements of RandomAccessIterator, which it does
            in this case, std::distance is constant, not linear. */
-
-        auto itrRHS = std::lower_bound(arrayInput + pivot, arrayInput + len, elem);
+        return std::distance(arrayInput, itrLHS);
 
     }
-
-    int dummy1 = 0;
-    return 0;
+    
+    auto itrRHS = std::lower_bound(arrayInput + pivot, arrayInput + len, elem);
+    if(itrRHS != arrayInput + len)
+        return std::distance(arrayInput, itrRHS);
+    else
+        return 0; //error; not found
 }
 
 /* Fun Note:
@@ -70,16 +78,28 @@ inline size_t findElement(T const* arrayInput, size_t const len, T const& elem)
    (not sure about c-arrays), but surely std::arrays. This could be used to 'fix'
    the array here, also probably useful in the sorting implementations - e.g., quicksort
    with a lambda function for the < operator.
-
-
-   
    */
 
 int main()
 {
     //                     V
     int    arr[6] = {7, 8, 9, 10, 4, 5};
-    size_t find = findElement(arr, 6,  9);
+    size_t find = findElement(arr, 6,  4);
+    assert(find == 4);
+    size_t find1 = findElement(arr, 6, 7);
+    assert(find1 == 0);
+    size_t find2 = findElement(arr, 6, 5);
+    assert(find2 == 5);
+    size_t find3 = findElement(arr, 6, 10);
+    assert(find3 == 3);
+
+    int arr2[7] = {199, 2, 16, 22, 41, 55, 60};
+    size_t find10 = findElement(arr2, 7, 199);
+    assert(find10 == 0);
+    size_t find11 = findElement(arr2, 7, 60);
+    assert(find11 == 6);
+    size_t find12 = findElement(arr2, 7, 22);
+    assert(find12 == 3);
 
     //assuming increasing order, if element 2 < element 1, you're 'around' the pivot.
 
