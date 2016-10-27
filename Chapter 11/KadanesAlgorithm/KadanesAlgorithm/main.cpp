a73c60d17f48e15e04043bdbece1758add16dc0c
Viper-PC\Viper <kreppsjs@gmail.com>
2016-10-27 14:51:11 -0500

    kadane's algorithm implemented, minor testing

--- Chapter 11/KadanesAlgorithm/KadanesAlgorithm/main.cpp
+++ Chapter 11/KadanesAlgorithm/KadanesAlgorithm/main.cpp
@@ -9,19 +9,34 @@
    
    Kadane's algorithm will, using dynamic programming, track previous
    maximum subarrays while traversing the input by terminus element. */
+#include <algorithm>
+#include <cassert>
+
 template <class T>
-inline void largestSum( T const*const  input,
-                        size_t const   length )
+inline T largestSum( T const*const  input,
+                     size_t const   length )
 {
+    assert(input && length > 0);
 
+    T      maxGlobal = input[0], maxCurrent = input[0];
+    for(size_t ii = 1; ii < length; ++ii)
+    {
+        /* The local max will either be the element or the max global + the element */
+        /* The global max will remain the same or be swapped with the local */
+        maxCurrent = std::max(input[ii] + maxCurrent, input[ii]);
+        maxGlobal  = std::max(maxCurrent, maxGlobal);
+    }
 
-
+    return maxGlobal;
 }
 
 /* What if all the elements are negative? */
 int main()
 {
     int arr[8] = {-2, -3, 4, -1, -2, 1, 5, -3};
+    int sum = largestSum(arr, 8);
+    assert(sum == 7);
 
+    int dummy = 0;
 
 }
