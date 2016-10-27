e5c6c2b8b59986ff41b6988ab575e11ed4fb8e1d
Viper-PC\Viper <kreppsjs@gmail.com>
2016-10-27 15:11:21 -0500

    additional testing

--- Chapter 11/KadanesAlgorithm/KadanesAlgorithm/main.cpp
+++ Chapter 11/KadanesAlgorithm/KadanesAlgorithm/main.cpp
@@ -8,7 +8,14 @@
    terminating at element N. 
    
    Kadane's algorithm will, using dynamic programming, track previous
-   maximum subarrays while traversing the input by terminus element. */
+   maximum subarrays while traversing the input by terminus element.
+   
+   Apparently Kadane's algorithm hardcodes a zero value to compare against,
+   which is just ametuerish. This algorithm can easily and clearly be made
+   generic.
+   
+   I don't find it acceptable that the scientific community would overlook
+   such a simple error with such a simple solution. */
 #include <algorithm>
 #include <cassert>
 
@@ -30,13 +37,25 @@ inline T largestSum( T const*const  input,
     return maxGlobal;
 }
 
-/* What if all the elements are negative? */
+/* What if all the elements are negative?
+   What if you want to return the range, not just the sum? */
 int main()
 {
     int arr[8] = {-2, -3, 4, -1, -2, 1, 5, -3};
     int sum = largestSum(arr, 8);
     assert(sum == 7);
 
-    int dummy = 0;
+    int arr2[5] = {-10, -6, -1, -30, -9};
+    int sum2 = largestSum(arr2, 5);
+    assert(sum2 == -1);
 
+    int arr3[5] = {1, -5, 2, -1, 3};
+    int sum3 = largestSum(arr3, 5);
+    assert(sum3 = 4);
+
+    int arr4[9] = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    int sum4 = largestSum(arr4, 9);
+    assert(sum4 == 6);
+
+    int dummy = 3;
 }
