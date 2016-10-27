b2e0c4ea20343ed223d924c8dc08ddc1891b8598
Viper-PC\Viper <kreppsjs@gmail.com>
2016-10-27 13:24:28 -0500

    outline kadane approach

--- Chapter 11/KadanesAlgorithm/KadanesAlgorithm/main.cpp
+++ Chapter 11/KadanesAlgorithm/KadanesAlgorithm/main.cpp
@@ -2,7 +2,26 @@
    
    Solutions by: vnpjeremy */
 
+
+/* Certainly we can do this by evaluating -all- the subarrays of the
+   input. This will take O(n^2) time. Could evaluate all subarrays
+   terminating at element N. 
+   
+   Kadane's algorithm will, using dynamic programming, track previous
+   maximum subarrays while traversing the input by terminus element. */
+template <class T>
+inline void largestSum( T const*const  input,
+                        size_t const   length )
+{
+
+
+
+}
+
+/* What if all the elements are negative? */
 int main()
 {
+    int arr[8] = {-2, -3, 4, -1, -2, 1, 5, -3};
+
 
 }
