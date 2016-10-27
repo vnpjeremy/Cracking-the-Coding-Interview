15bac8527c4b1217b35f43f49172406f781dda2a
Viper-PC\Viper <kreppsjs@gmail.com>
2016-10-27 15:43:33 -0500

    return the range too, still has a bug

--- Chapter 11/KadanesAlgorithm/KadanesAlgorithm/main.cpp
+++ Chapter 11/KadanesAlgorithm/KadanesAlgorithm/main.cpp
@@ -18,6 +18,7 @@
    such a simple error with such a simple solution. */
 #include <algorithm>
 #include <cassert>
+#include <iterator>
 
 template <class T>
 inline T largestSum( T const*const  input,
@@ -37,6 +38,35 @@ inline T largestSum( T const*const  input,
     return maxGlobal;
 }
 
+template <class T>
+inline T largestSum( T /*const*/*       input,
+                     size_t const   length,
+                     T /*const*/**       begSubArray,
+                     T /*const*/**       endSubArray )
+{
+    assert(input && length > 0);
+
+    T      maxGlobal = input[0], maxCurrent = input[0];
+    for(size_t ii = 1; ii < length; ++ii)
+    {
+        if(input[ii] + maxCurrent < input[ii])
+        {
+            maxCurrent = input[ii];
+            *begSubArray = input + ii;
+            *endSubArray = input + ii;
+        }
+        else
+        {
+            maxCurrent = input[ii] + maxCurrent;
+            *endSubArray = input + ii;
+        }
+
+        maxGlobal  = std::max(maxCurrent, maxGlobal);
+    }
+
+    return maxGlobal;
+}
+
 /* What if all the elements are negative?
    What if you want to return the range, not just the sum? */
 int main()
@@ -44,6 +74,10 @@ int main()
     int arr[8] = {-2, -3, 4, -1, -2, 1, 5, -3};
     int sum = largestSum(arr, 8);
     assert(sum == 7);
+
+    int *beg = nullptr, *end = nullptr;
+    int sumDebug = largestSum(arr, 8, &beg, &end);
+    assert(*beg == 4 && *end == -3);
 
     int arr2[5] = {-10, -6, -1, -30, -9};
     int sum2 = largestSum(arr2, 5);
@@ -56,6 +90,10 @@ int main()
     int arr4[9] = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
     int sum4 = largestSum(arr4, 9);
     assert(sum4 == 6);
+
+    int *beg2 = nullptr, *end2 = nullptr;
+    int sumDebug2 = largestSum(arr4, 9, &beg2, &end2);
+    assert(*beg2 == 4 && *end2 == -5);
 
     int dummy = 3;
 }
