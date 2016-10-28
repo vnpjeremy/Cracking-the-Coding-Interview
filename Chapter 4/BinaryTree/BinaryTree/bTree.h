c7130dfe99d7958d9b2b5ca537878e0e7ae7b7a2
Viper-PC\Viper <kreppsjs@gmail.com>
2016-10-28 14:50:40 -0500

    incremental skeleton

--- Chapter 4/BinaryTree/BinaryTree/bTree.h
+++ Chapter 4/BinaryTree/BinaryTree/bTree.h
@@ -1,6 +1,65 @@
 #pragma once
 
+template <class T>
 class bTree
 {
+public:
+    bTree() :
+        m_root()
+    {
 
+    }
+
+private:
+    template <class T>
+    struct bTreeNode
+    {
+        bTreeNode() :
+            m_val(),
+            m_lhs(),
+            m_rhs()
+        {
+        }
+
+        bTreeNode(T const& value) :
+            m_val(value),
+            m_lhs(),
+            m_rhs()
+        {
+        }
+
+        T              m_val;
+        bTreeNode<T>   *m_lhs;
+        bTreeNode<T>   *m_rhs;
+    };
+
+    bTreeNode<T>   *m_root;
+
+public:
+    void insert(T const&              value)
+    {       
+        if(!m_root)
+            m_root = new bTreeNode<T>(value);
+        else
+        {
+            //    //T             parentVal = m_root->m_val;
+            //    
+            bTreeNode<T>  *cur = m_root;
+            while(cur)
+            {
+                if(value < cur->m_val)
+                    cur = cur->m_lhs;
+                else
+                    cur = cur->m_rhs;
+            }
+            cur = new bTreeNode<T>(value);
+            /* leaf node */
+            //if(value < parentVal)
+        }
+    }
+
+    ~bTree()
+    {
+        bTreeNode<T>* cur = m_root;
+    }
 };
--- Chapter 4/BinaryTree/BinaryTree/main.cpp
+++ Chapter 4/BinaryTree/BinaryTree/main.cpp
@@ -1,8 +1,18 @@
 #include "bTree.h"
+#include <vector>
 
 int main()
 {
-    bTree t1;
+    bTree<int> t1;
+
+    std::vector<int> input = {11, 6, 14, 18, 5, 8, 10};
+        /*  10
+          /   \
+         6    14 
+        / \   / \ 
+       5   8 11  18 */
+    for(auto elm : input)
+        t1.insert(elm);
 
     int dummy = 0;
 }
