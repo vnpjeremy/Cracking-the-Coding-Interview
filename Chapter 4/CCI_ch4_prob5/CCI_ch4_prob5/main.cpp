358ee08ecc80943ca1bb93d491d18e7b1bf464a8
Viper-PC\Viper <kreppsjs@gmail.com>
2016-11-02 19:51:30 -0500

    skeleton of ch4 prob5

---
+++ Chapter 4/CCI_ch4_prob5/CCI_ch4_prob5/main.cpp
@@ -0,0 +1,15 @@
+/* Cracking the Coding Interview, McDowell 5th Ed, 2011
+   Chapter 4: Problem 5.
+   
+   Create a check to query if a BT is a BST.
+   
+   Solutions by: vnpjeremy*/
+
+#include "bst.h"
+
+int main()
+{
+    bTree<int> tree1;
+
+    int dummy = 0;
+}
