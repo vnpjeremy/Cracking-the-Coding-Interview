5e486cc11a784a3d3bb21b8311667125b8f25c7d
Viper-PC\Viper <kreppsjs@gmail.com>
2016-11-05 12:36:13 -0500

    skeleton of ch4 prob1

---
+++ Chapter 4/CCI_ch4_prob1/CCI_ch4_prob1/main.cpp
@@ -0,0 +1,15 @@
+/* Cracking the Coding Interview, McDowell 5th Ed, 2011
+   Chapter 4: Problem 1.
+   
+   Make a check to verify if a BT is balanced.
+   
+   Solutions by: vnpjeremy*/
+
+
+/* Essentially they are asking for you to perform a depth-first traversal
+   and keep track of the depth at each leaf node. Compare the min/max and
+   return a result. */
+int main()
+{
+
+}
