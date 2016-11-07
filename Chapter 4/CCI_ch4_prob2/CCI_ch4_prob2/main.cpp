77d3c2bd356ebc46ef75c16c91a54c3539e299cf
Viper-PC\Viper <kreppsjs@gmail.com>
2016-11-06 18:25:37 -0600

    skeleton of ch4 prob2

---
+++ Chapter 4/CCI_ch4_prob2/CCI_ch4_prob2/main.cpp
@@ -0,0 +1,11 @@
+/* Cracking the Coding Interview, McDowell 5th Ed, 2011
+   Chapter 4: Problem 2.
+   
+   Find a route between two nodes, if possible, in a directed graph.
+   
+   Solutions by: vnpjeremy*/
+
+int main()
+{
+
+}
