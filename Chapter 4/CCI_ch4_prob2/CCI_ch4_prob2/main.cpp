e32bf04d9925a2e917161cb8328b8e19ede9dc28
Viper-PC\Viper <kreppsjs@gmail.com>
2016-11-07 23:39:33 -0600

    commenting on approach

--- Chapter 4/CCI_ch4_prob2/CCI_ch4_prob2/main.cpp
+++ Chapter 4/CCI_ch4_prob2/CCI_ch4_prob2/main.cpp
@@ -5,7 +5,26 @@
    
    Solutions by: vnpjeremy*/
 
+/* A graph G is an ordered pair of a set of V vertices and E edges 
+   G = (V,E) 
+   
+   A directed graph is one which consists of edges that have directionality,
+   e.g., v->u. 
+   
+   A tree is a directed graph as we will have a path from the root to a leaf
+   node, but not vice-versa. 
+   
+   A walk is a route. A 'path' is a simple walk; a walk in which no edges or 
+   vertices are repated. Assume this. 
+   
+   A 'trail' is a walk except repeated vertices are allowed, but not edges.
+   
+   A graph with no cycle is acyclic. A tree is a directed acyclic graph. (DAG) */
 int main()
 {
-
+    /* Note that a graph will be programmatically implemented differently than
+       a simple thing like a BST or BT. The member data will be an iterable,
+       not a non-connected series of N (2, in BST) 
+       
+       Note also that a BT is not strongly connected. */
 }
